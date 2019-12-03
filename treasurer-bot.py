72fb7e7076d117e51d047b1886b69500da35e555
Robby Hemawan Pramudito <pramuditorh@gmail.com>
2019-12-03 16:39:13 +0700

    credentials.py

--- treasurer-bot.py
+++ treasurer-bot.py
@@ -1,7 +1,8 @@
 from telegram.ext import Updater, CommandHandler
+from credentials import TELEGRAM_TOKEN
 import logging
 
-updater = Updater(token='1032606426:AAE2fXbgpOc2N7dY7kURt__jCWC0KedTQZY', use_context=True)
+updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -11,6 +12,12 @@ def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, 
                     text="I'm a bot, please talk to me!")
 
+def kas(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, 
+                    text="Kas")
+
 start_handler = CommandHandler('start', start)
+kas_handler = CommandHandler('kas', kas)
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(kas_handler)
 updater.start_polling()
