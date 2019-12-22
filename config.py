2b4e0850e593315f13c864f40d60101afa4fedc7
Robby Hemawan Pramudito <pramuditorh@gmail.com>
2019-12-22 13:40:32 +0700

    add config.py to heroku

---
+++ config.py
@@ -0,0 +1,3 @@
+TELEGRAM_TOKEN = '1032606426:AAE2fXbgpOc2N7dY7kURt__jCWC0KedTQZY' # Telegram Token from BotFather
+GDRIVE_CRED = 'client_secret.json' # Credentials you get from Google API
+GDRIVE_SHEET_NAME = '[19/20] PEMASUKAN, PENGELUARAN, dan KAS CN'
