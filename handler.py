586b030ccdc7463df25cf0f516d4050c5d115bb6
Robby Hemawan Pramudito <pramuditorh@gmail.com>
2019-12-30 21:29:09 +0700

    give comment to variable nama

--- handler.py
+++ handler.py
@@ -10,9 +10,12 @@ def start(update, context):
 def kas(update, context):
     kas_gsheet = gsheet.getKas()
     text = update.message.text.split()
-    bulan = text[1].upper()
+    #nama = text[1].capitalize()
+    bulan = text[2].upper()
     df = pd.DataFrame(kas_gsheet)
     df.index += 1
+
+    print(kas_gsheet)
 
     context.bot.send_message(chat_id=update.effective_chat.id,
                             text="Bayar Kas\n{}".format(df[['NAMA', bulan]]))
